9b062cedad4b6f0cbca5e03acc58de0d3647eda2
Hamed <erghezi@gmail.com>
2020-03-24 01:36:41 +0430

    Add Support to Catalina Music App, Musixmatch and Spotify

--- Lyrics Widget/LyricsFinder.m
+++ Lyrics Widget/LyricsFinder.m
@@ -24,6 +24,30 @@
   return ((NSString *) CFBridgingRelease(escapedURI));
 }
 
+
+/**
+ * create musixmatch URL
+ */
++(NSString *) createmusixmatchUrl:(NSString *)title by:(NSString *)artist
+{
+    NSString *url = @"https://www.musixmatch.com/lyrics/";
+
+    title = [title stringByTrimmingCharactersInSet:[NSCharacterSet punctuationCharacterSet]];
+    artist = [artist stringByTrimmingCharactersInSet:[NSCharacterSet punctuationCharacterSet]];
+    title = [title stringByReplacingOccurrencesOfString:@"\'" withString:@"-"];
+    artist = [artist stringByReplacingOccurrencesOfString:@"\'" withString:@"-"];
+    
+    NSError *error = nil;
+    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"^ebi" options:NSRegularExpressionCaseInsensitive error:&error];
+    artist = [regex stringByReplacingMatchesInString:artist options:0 range:NSMakeRange(0, [artist length]) withTemplate:@"ebi-2"];
+
+    url = [url stringByAppendingString:[LyricsFinder escapeUri:artist withSeparator:@"-"]];
+    url = [url stringByAppendingString:@"/"];
+    url = [url stringByAppendingString:[LyricsFinder escapeUri:title withSeparator:@"-"]];
+    url = [url stringByAppendingString:@"/"];
+    return url;
+}
+
 /**
  * create azlyrics URL
  */
@@ -64,10 +88,77 @@
   return url;
 }
 
+
 /**
  * download lyrics from given URL. returns nil, if lyrics not found
  */
-+ (NSString *)downloadazlyricsLyricsFrom:(NSString *)url
++ (NSString *)downloadLyricsFromMusixmatch:(NSString *)url
+{
+    NSLog(@"downloading '%@'", url);
+         
+    NSURL *requestUrl = [NSURL URLWithString:url];
+    
+    NSString* userAgent = @"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36";
+    NSMutableURLRequest* request = [[NSMutableURLRequest alloc] initWithURL:requestUrl];
+    [request setValue:userAgent forHTTPHeaderField:@"User-Agent"];
+
+    NSURLResponse* response = nil;
+    NSError* error = nil;
+    NSData* data = [NSURLConnection sendSynchronousRequest:request
+                                         returningResponse:&response
+                                                     error:&error];
+    if (data == nil) {
+        return nil;
+    }
+    
+    NSString *content = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    
+    content = [content stringByReplacingOccurrencesOfString:@"lyrics__content__warning" withString:@"lyrics__content__ok"];
+    
+    NSString* scanString = @"";
+    NSString* scanString2 = @"";
+    NSString *startTag= @"<span class=\"lyrics__content__ok\">";
+    NSString *endTag= @"</span>";
+    
+
+    
+    if ([content rangeOfString:startTag].location == NSNotFound) {
+        return nil;
+    }
+    
+    if (content.length > 0) {
+        
+        NSScanner* scanner = [[NSScanner alloc] initWithString:content];
+        
+        @try {
+            [scanner scanUpToString:startTag intoString:nil];
+            scanner.scanLocation += [startTag length];
+            [scanner scanUpToString:endTag intoString:&scanString];
+            [scanner scanUpToString:startTag intoString:nil];
+            scanner.scanLocation += [startTag length];
+            [scanner scanUpToString:endTag intoString:&scanString2];
+        }
+        @catch (NSException *exception) {
+            return nil;
+        }
+        @finally {
+            NSString *returnString = [ scanString stringByAppendingString:@"\n" ];
+            returnString = [returnString stringByAppendingString:scanString2];
+            return returnString;
+            
+        }
+        
+    }
+    
+    
+  
+ 
+}
+
+/**
+ * download lyrics from given URL. returns nil, if lyrics not found
+ */
++ (NSString *)downloadLyricsFromAzlyrics:(NSString *)url
 {
   NSLog(@"downloading '%@'", url);
   
@@ -131,7 +222,7 @@
 /**
  * download lyrics from given URL. returns nil, if lyrics not found
  */
-+ (NSString *)downloadLyricsWikiaLyricsFrom:(NSString *)url
++ (NSString *)downloadLyricsFromWikia:(NSString *)url
 {
   NSLog(@"downloading '%@'", url);
   NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:url]];
@@ -142,9 +233,10 @@
   NSXMLDocument *document = [[NSXMLDocument alloc] initWithData:data
                                                         options:NSXMLDocumentTidyHTML
                                                           error:nil];
-  if (document == nil) {
-    return nil;
-  }
+
+    if (document == nil) {
+      return nil;
+    }
   
   NSMutableArray *lyricLines = [NSMutableArray new];
   NSString *content = [document stringValue];
@@ -161,7 +253,7 @@
       isLyric = TRUE;
       firstLyricLine = TRUE;
     }
-    else if ([line isEqualToString:@"External linksNominate as Song of the Day"]) {
+    else if ([line containsString:@"External linksNominate as Song of the Day"]) {
       // first line after lyrics
       isLyric = FALSE;
     }
@@ -205,20 +297,28 @@
   NSString *url;
   
   // try do download lyrics from musixmatch.com using given title and artist
+  url = [LyricsFinder createmusixmatchUrl:title by:artist];
+  lyrics = [LyricsFinder downloadLyricsFromMusixmatch:url];
+
+  if (lyrics == nil){
+
+ // try do download lyrics from musixmatch.com using given title and artist
   url = [LyricsFinder createazLyricsUrl:title by:artist];
-  lyrics = [LyricsFinder downloadazlyricsLyricsFrom:url];
-  if (lyrics != nil)
-    return lyrics;
-  
+  lyrics = [LyricsFinder downloadLyricsFromAzlyrics:url];
+  }
+
+  if (lyrics == nil){
+
   // try do download lyrics from lyrics.wikia.com using given title and artist
   url = [LyricsFinder createLyricsWikiaUrlFor:title by:artist];
-  lyrics = [LyricsFinder downloadLyricsWikiaLyricsFrom:url];
-  if (lyrics != nil)
-    return lyrics;
-  
+  lyrics = [LyricsFinder downloadLyricsFromWikia:url];
+  }
+  if (lyrics == nil){
+
   // try to download lyrics from lyrics.wikia.com using given artist and capitalized title
   url = [LyricsFinder createLyricsWikiaUrlFor:[title capitalizedString] by:artist];
-  lyrics = [LyricsFinder downloadLyricsWikiaLyricsFrom:url];
+  lyrics = [LyricsFinder downloadLyricsFromWikia:url];
+  }
   if (lyrics != nil)
     return lyrics;
   
--- Lyrics Widget/TodayViewController.m
+++ Lyrics Widget/TodayViewController.m
@@ -2,19 +2,25 @@
 //  TodayViewController.m
 //  Lyrics Widget
 //
-//  Created by Hamed on 5/24/16.
-//  Copyright © 2016 Hamed. All rights reserved.
+//  Created by Hamed on 9/9/1398 AP.
+//  Copyright © 1398 Hamed. All rights reserved.
 //
 
 #import "TodayViewController.h"
 #import <NotificationCenter/NotificationCenter.h>
 #import "iTunes.h"
+#import "Music.h"
+#import "Spotify.h"
 #import "LyricsFinder.h"
 
 
 @interface TodayViewController () <NCWidgetProviding>
 
+@property (nonatomic, readonly) MusicApplication *MusicApp;
 @property (nonatomic, readonly) iTunesApplication *iTunesApp;
+@property (nonatomic, readonly) SpotifyApplication *SpotifyApp;
+
+
 @property (nonatomic, weak) IBOutlet NSImageView *artworkImageView;
 
 @property (nonatomic, weak) IBOutlet NSTextField *trackNameLabel;
@@ -22,6 +28,8 @@
 @property (nonatomic, weak) IBOutlet NSTextField *artistNameAndAlbumNameLabel;
 @property (nonatomic, weak) IBOutlet NSTextField *trackLyrics;
 
+@property (weak) IBOutlet NSLayoutConstraint *imagewidth;
+@property (weak) IBOutlet NSLayoutConstraint *imageheight;
 
 @end
 
@@ -30,7 +38,7 @@
 
 - (void)viewWillAppear
 {
-    [[NSDistributedNotificationCenter defaultCenter] addObserver:self selector:@selector(updateView:) name:@"com.apple.iTunes.playerInfo" object:nil];
+    [[NSDistributedNotificationCenter defaultCenter] addObserver:self selector:@selector(updateView:) name:@"com.apple.Music.playerInfo" object:nil];
 }
 
 - (void)viewWillDisappear
@@ -44,8 +52,72 @@
 - (void)updateView:(NSNotification *)notification
 {
     
-    iTunesTrack *track = self.iTunesApp.currentTrack;
-    if(self.iTunesApp.running && track.persistentID) {
+    if(self.MusicApp.running && self.MusicApp.playerState == MusicEPlSPlaying) {
+        MusicTrack *track = self.MusicApp.currentTrack;
+        self.view.hidden = NO;
+        
+        NSString *trackName = track.name;
+        NSString *artistName = track.artist;
+        NSString *albumName = track.album;
+        
+        self.trackNameLabel.stringValue = trackName ? trackName : @"";
+        self.artistNameAndAlbumNameLabel.stringValue = [NSString stringWithFormat:@"%@%@%@", artistName ? artistName : @"", (artistName.length && albumName.length) ? @" - " : @"", albumName ? albumName : @""];
+        
+        
+        
+        if (![[[self.MusicApp currentTrack] lyrics]  isEqual: @""] ){
+            
+            self.trackLyrics.stringValue = [[self.MusicApp currentTrack] lyrics];
+            
+        }
+        else{
+            
+            self.trackLyrics.stringValue =  @"" ;
+            
+            
+            dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0ul);
+            dispatch_async(queue, ^{
+                NSString *lyrics = [LyricsFinder findLyricsOf:[track name]by:[track artist]];
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    self.trackLyrics.stringValue =  lyrics ;
+                    if (![lyrics isEqual: @""] ){
+                        
+                        
+                        track.lyrics=lyrics;
+                        
+                    }
+                    
+                });
+            });
+            
+            
+        }
+        
+        
+        MusicArtwork *artwork = track.artworks.firstObject;
+        NSImage *artworkImage = [[NSImage alloc] init];
+        
+        // For some reason in the Music app sometimes the expected return of NSImage from trackArt.data
+        // is instead an NSAppleEventDescriptor containing raw data otherwise it's an image like normal.
+        // Also trackArt.rawData appears to be no longer used and empty :(
+        if ([artwork.data.className isEqualToString:@"NSAppleEventDescriptor"]) {
+            NSAppleEventDescriptor *t = (NSAppleEventDescriptor*)artwork.data;
+            NSData *d = t.data;
+            artworkImage = [[NSImage alloc] initWithData:d];
+        } else {
+            artworkImage = artwork.data;
+        }
+        
+                
+        if(!artworkImage) {
+            NSImage *placeholder = [NSImage imageNamed:@"musicArtwork"];
+            artworkImage = placeholder;
+        }
+        self.artworkImageView.image = artworkImage;
+
+    }
+    else if(self.iTunesApp.running && self.iTunesApp.playerState == iTunesEPlSPlaying) {
+        iTunesTrack *track = self.iTunesApp.currentTrack;
         self.view.hidden = NO;
         
         NSString *trackName = track.name;
@@ -71,7 +143,7 @@
             dispatch_async(queue, ^{
                 NSString *lyrics = [LyricsFinder findLyricsOf:[track name]by:[track artist]];
                 dispatch_async(dispatch_get_main_queue(), ^{
-                    self.trackLyrics.stringValue =  lyrics ;                    
+                    self.trackLyrics.stringValue =  lyrics ;
                     if (![lyrics isEqual: @""] ){
                         
                         
@@ -90,19 +162,71 @@
         
         NSImage *artworkImage = [[NSImage alloc] initWithData:artwork.rawData];
         if(!artworkImage) {
-            NSImage *placeholder = [NSImage imageNamed:@"ArtworkPlaceholder"];
+            NSImage *placeholder = [NSImage imageNamed:@"musicArtwork"];
+            artworkImage = placeholder;
+        }
+        self.artworkImageView.image = artworkImage;
+        
+    }
+    else if (self.SpotifyApp.running && self.SpotifyApp.playerState == SpotifyEPlSPlaying) {
+        self.view.hidden = NO;
+        SpotifyTrack *spotifytrack = self.SpotifyApp.currentTrack;
+        NSString *trackName = spotifytrack.name;
+        NSString *artistName = spotifytrack.artist;
+        NSString *albumName = spotifytrack.album;
+        
+        
+        NSError *error = NULL;
+        NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@" -.*remaster.*" options:NSRegularExpressionCaseInsensitive error:&error];
+        
+        trackName = [regex stringByReplacingMatchesInString:trackName options:0 range:NSMakeRange(0, [trackName length]) withTemplate:@""];
+        
+        self.trackNameLabel.stringValue = trackName ? trackName : @"";
+        self.artistNameAndAlbumNameLabel.stringValue = [NSString stringWithFormat:@"%@%@%@", artistName ? artistName : @"", (artistName.length && albumName.length) ? @" - " : @"", albumName ? albumName : @""];
+
+        self.trackLyrics.stringValue =  @"" ;
+        
+        
+        dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0ul);
+        dispatch_async(queue, ^{
+            NSString *lyrics = [LyricsFinder findLyricsOf:trackName by:artistName];
+            dispatch_async(dispatch_get_main_queue(), ^{
+                self.trackLyrics.stringValue =  lyrics ;
+                
+            });
+        });
+        
+        NSLog(@"artwork '%@'", spotifytrack.artworkUrl);
+
+        NSImage *artworkImage = [[NSImage alloc] initWithContentsOfURL: [NSURL URLWithString: spotifytrack.artworkUrl]];
+
+        
+        if(!artworkImage) {
+            NSImage *placeholder = [NSImage imageNamed:@"musicArtwork"];
             artworkImage = placeholder;
         }
         self.artworkImageView.image = artworkImage;
     }
     else {
         self.view.hidden = YES;
+        self.imageheight.constant=0;
+        self.imagewidth.constant=0;
     }
 }
 
 
 #pragma mark -
 #pragma mark Accessor Method
+- (MusicApplication *)MusicApp
+{
+    static MusicApplication *MusicApp = nil;
+    if(!MusicApp) {
+        MusicApp = [SBApplication applicationWithBundleIdentifier:@"com.apple.Music"];
+        
+    }
+    return MusicApp;
+}
+
 - (iTunesApplication *)iTunesApp
 {
     static iTunesApplication *iTunesApp = nil;
@@ -113,6 +237,15 @@
     return iTunesApp;
 }
 
+- (SpotifyApplication *)SpotifyApp
+{
+    static SpotifyApplication *SpotifyApp = nil;
+    if(!SpotifyApp) {
+        SpotifyApp = [SBApplication applicationWithBundleIdentifier:@"com.spotify.client"];
+        
+    }
+    return SpotifyApp;
+}
 
 - (void)widgetPerformUpdateWithCompletionHandler:(void (^)(NCUpdateResult result))completionHandler {
     [self updateView:nil];
